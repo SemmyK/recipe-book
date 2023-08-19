909f0f6401a0f1107f950e1b0458777bcbe248a7
SemmyK <66922979+SemmyK@users.noreply.github.com>
2023-08-19 19:03:26 +0200

    added express.static

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ const app = express()
 //add middleware (body-parser)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static('../frontend'))
 
 //create the route
 app.get('/', (req, res) => {
