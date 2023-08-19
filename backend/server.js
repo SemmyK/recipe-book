d7a80a0b224088ec0821da45c22fc1483b24aa9f
SemmyK <66922979+SemmyK@users.noreply.github.com>
2023-08-20 00:46:40 +0200

    updated server side home page

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,9 @@ app.use(
 
 //create the route
 app.get('/', (req, res) => {
-	res.send('hello')
+	res.send(
+		'<div style="margin-top:10em; text-align:center;display:flex;justify-conten:center;align-items:center;height:80vh"> <h1>Welcome to RecipeBook server side</h1></div>'
+	)
 })
 
 //use route created in routes
