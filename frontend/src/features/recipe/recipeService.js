88affc0fe60bfa2f3e57abf0c5704cff1909f124
SemmyK <66922979+SemmyK@users.noreply.github.com>
2023-08-20 01:21:33 +0200

    fixed path

--- frontend/src/features/recipe/recipeService.js
+++ frontend/src/features/recipe/recipeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = `${process.env.REACT_APP_SERVER_URL}/api/recipes`
+const API_URL = `${process.env.REACT_APP_SERVER_URL}api/recipes`
 
 //get all recipes
 const getAllRecipes = async () => {
